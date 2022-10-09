ceea0d9464184e3fe4238dec4039c9c5afc8adfd
Davy Souza <davy.souza@outlook.com>
2022-10-09 17:01:14 -0300

    Add index.html

--- buson_app/views.py
+++ buson_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, BusOn!")
+    return render(request, 'buson_app/index.html')
--- buson_project/settings.py
+++ buson_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'buson_app.apps.BusonAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
