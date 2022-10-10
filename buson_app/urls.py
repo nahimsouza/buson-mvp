3ea1f3c007aca1d16c20005412490eaae4eb8982
Davy Souza <davy.souza@outlook.com>
2022-10-10 13:53:17 -0300

    Add all remaining pages (configuracoes, rotas and cadastro-colaborador)

--- buson_app/urls.py
+++ buson_app/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'buson_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('configuracoes', views.configuracoes, name='configuracoes'),
+    path('rotas', views.rotas, name='rotas'),
+    path('cadastrar-colaborador', views.cadastrarColaborador, name='cadastrar-colaborador'),
 ]
--- buson_app/views.py
+++ buson_app/views.py
@@ -5,3 +5,12 @@ from django.template import loader
 
 def index(request):
     return render(request, 'buson_app/index.html')
+
+def configuracoes(request):
+    return render(request, 'buson_app/configuracoes.html')
+
+def rotas(request):
+    return render(request, 'buson_app/rotas.html')
+
+def cadastrarColaborador(request):
+    return render(request, 'buson_app/cadastro-colaborador.html')
